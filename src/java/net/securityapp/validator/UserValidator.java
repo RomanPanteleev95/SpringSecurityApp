66a2b454170a2775ffe21fc562623ca043af8083
Роман Пантелеев <panteleevrmn95@gmail.com>
2018-07-02 23:59:31 +0400

    Add controllers

---
+++ src/java/net/securityapp/validator/UserValidator.java
@@ -0,0 +1,32 @@
+package net.securityapp.validator;
+
+import net.securityapp.model.User;
+import net.securityapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "Required");
+        if (user.getUsername().length() < 8 || user.getUsername().length() > 15){
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+    }
+}
